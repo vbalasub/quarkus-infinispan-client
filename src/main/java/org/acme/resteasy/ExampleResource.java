749a7254ad237be7eb63544718e9e5fac133e53a
Vijay Balasubramanian <vbalasub@localhost.localdomain>
2020-11-05 14:01:42 -0600

    Quarkus openshift issue

---
+++ src/main/java/org/acme/resteasy/ExampleResource.java
@@ -0,0 +1,20 @@
+package org.acme.resteasy;
+
+
+import javax.enterprise.context.control.ActivateRequestContext;
+import javax.inject.Inject;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class ExampleResource implements QuarkusApplication {
+
+    @Override
+    public int run(String... args) throws Exception {
+        final String name = args.length > 0 ? String.join(" ", args) : "commando";
+        System.out.println("hello " + name);
+        return 0;
+    }
+}
